d164520e07686d87c8eb5bca28db119f303a3599
Rob Mayer <Rob@Robinomicon.com>
2015-03-09 00:42:07 +0000

    Update minerva.blendmodes.js

--- minerva.blendmodes.js
+++ minerva.blendmodes.js
@@ -99,15 +99,15 @@ Minerva.blendmodes.inversesubtract = function(a, b) {
 };
 
 Minerva.blendmodes.arctangent = function(a, b) {
-	return ( (a == 0) ? ( b == 0 ? 0 : 1 ) : 2 * Math.atan(a / b) / Math.PI );
+	return ( (a==0) ? ( (b==0)?0:1 ) : 2*Math.atan(b/a)/Math.PI );
 };
 
 Minerva.blendmodes.parallel = function(a, b) {
-	return ( (a==0) ? 1 : (b == 0 ? 1 : (2 / (1/a + 1/b)) ) );
+	return (2 * 1 / ( ((b == 0) ? 1 : 1/b) + ((a == 0) ? 1 : 1/a) ));
 };
 
 Minerva.blendmodes.equivalence = function(a, b) {
-	return (1 - Math.abs(a - b));
+	return (Math.abs(b - a));
 };
 
 Minerva.blendmodes.grainmerge = function(a, b) {
@@ -135,7 +135,10 @@ Minerva.blendmodes.geometricmean = function(a, b) {
 };
 
 Minerva.blendmodes.softlightsvg = function(a, b) {
-	return ( (a > 0.5) ? b + (2*a - 1) * ( (b > 0.25 ? Math.sqrt(b) : ((16*b-12)*b+4)) - b) : (b - (1 - 2 * a) * b * (1-b)) );
+	if (b > 0.5) {
+		return a + (2*b-1)*( ((a > 0.25) ? Math.sqrt(a) : ((16*a - 12)*a + 4)*a)-a);
+	}
+	return (a-(1-2*b)*a*(1-a));
 };
 
 Minerva.blendmodes.overlay = function(a, b) {
@@ -143,7 +146,7 @@ Minerva.blendmodes.overlay = function(a, b) {
 };
 
 Minerva.blendmodes.softlight = function(a, b) {
-	return ( ( b<0.5 ) ? ( (1-2*b)*(a*a)+(2*b*a) ) : ( (1-(2*b-1))*a+(2*b-1)*Math.sqrt(a) ) );
+	return ( ( b<0.5 ) ? ( (1-2*b)*(a*a)+(2*b*a) ) : ( (1-(2*b-1))*a+(2*b-1)*Math.sqrt(a) ) );	
 };
 
 Minerva.blendmodes.peglight = function(a, b) {
@@ -163,7 +166,7 @@ Minerva.blendmodes.vividlight = function(a, b) {
 };
 
 Minerva.blendmodes.pinlight = function(a, b) {
-	return (b<2*a-1) ? (2*a-1) : ( (2*a-1<b) && (b<2*a)) ? b : 2*a;
+	return ( b > 0.5  ? (Math.max(a, 2*(b-0.5))) : (Math.min(a, 2 * b)));
 };
 
 Minerva.blendmodes.reflect = function(a, b) {
@@ -207,192 +210,254 @@ Minerva.blendmodes.binarynand = function(a, b) {
 };
 
 /* Channel Swaps */
-
-Minerva.mixmodes.rgbRed = function(a, b) {
+Minerva.mixmodes.hslhue = function(a, b) {
+	var hslA = Minerva.convert.rgb2hsl([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hslB = Minerva.convert.rgb2hsl([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsl2rgb([hslB[0], hslA[1], hslA[2]]);
 	return {
-		r: b.r,
-		g: a.g,
-		b: a.b,
-	};
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.rgbGreen = function(a, b) {
+Minerva.mixmodes.hslsaturation = function(a, b) {
+	var hslA = Minerva.convert.rgb2hsl([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hslB = Minerva.convert.rgb2hsl([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsl2rgb([hslA[0], hslB[1], hslA[2]]);
 	return {
-		r: a.r,
-		g: b.g,
-		b: a.b,
-	};
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.rgbBlue = function(a, b) {
+Minerva.mixmodes.hsllightness = function(a, b) {
+	var hslA = Minerva.convert.rgb2hsl([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hslB = Minerva.convert.rgb2hsl([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsl2rgb([hslA[0], hslA[1], hslB[2]]);
 	return {
-		r: a.r,
-		g: a.g,
-		b: b.b,
-	};
-};
-
-Minerva.mixmodes.cmykcyan = function(a, b) {
-	var cmykA = Minerva.convert.rgb2cmyk(a);
-	var cmykB = Minerva.convert.rgb2cmyk(b);
-	return Minerva.convert.cmyk2rgb({
-		c:cmykB.c,
-		m:cmykA.m,
-		y:cmykA.y,
-		k:cmykA.k,
-	});
-};
-
-Minerva.mixmodes.cmykmagenta = function(a, b) {
-	var cmykA = Minerva.convert.rgb2cmyk(a);
-	var cmykB = Minerva.convert.rgb2cmyk(b);
-	return Minerva.convert.cmyk2rgb({
-		c:cmykA.c,
-		m:cmykB.m,
-		y:cmykA.y,
-		k:cmykA.k,
-	});
-};
-
-Minerva.mixmodes.cmykyellow = function(a, b) {
-	var cmykA = Minerva.convert.rgb2cmyk(a);
-	var cmykB = Minerva.convert.rgb2cmyk(b);
-	return Minerva.convert.cmyk2rgb({
-		c:cmykA.c,
-		m:cmykA.m,
-		y:cmykB.y,
-		k:cmykA.k,
-	});
-};
-
-Minerva.mixmodes.cmykkey = function(a, b) {
-	var cmykA = Minerva.convert.rgb2cmyk(a);
-	var cmykB = Minerva.convert.rgb2cmyk(b);
-	return Minerva.convert.cmyk2rgb({
-		c:cmykA.c,
-		m:cmykA.m,
-		y:cmykA.y,
-		k:cmykB.k,
-	});
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
 Minerva.mixmodes.hsvhue = function(a, b) {
-	var hsvA = Minerva.convert.rgb2hsv(a);
-	var hsvB = Minerva.convert.rgb2hsv(b);
-	return Minerva.convert.hsv2rgb({
-		h:hsvB.h,
-		s:hsvA.s,
-		v:hsvA.v
-	});
+	var hsvA = Minerva.convert.rgb2hsv([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hsvB = Minerva.convert.rgb2hsv([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsv2rgb([hsvB[0], hsvA[1], hsvA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
 Minerva.mixmodes.hsvsaturation = function(a, b) {
-	var hsvA = Minerva.convert.rgb2hsv(a);
-	var hsvB = Minerva.convert.rgb2hsv(b);
-	return Minerva.convert.hsv2rgb({
-		h:hsvA.h,
-		s:hsvB.s,
-		v:hsvA.v
-	});
+	var hsvA = Minerva.convert.rgb2hsv([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hsvB = Minerva.convert.rgb2hsv([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsv2rgb([hsvA[0], hsvB[1], hsvA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
 Minerva.mixmodes.hsvvalue = function(a, b) {
-	var hsvA = Minerva.convert.rgb2hsv(a);
-	var hsvB = Minerva.convert.rgb2hsv(b);
-	return Minerva.convert.hsv2rgb({
-		h:hsvA.h,
-		s:hsvA.s,
-		v:hsvB.v
-	});
+	var hsvA = Minerva.convert.rgb2hsv([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hsvB = Minerva.convert.rgb2hsv([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsv2rgb([hsvA[0], hsvA[1], hsvB[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.hsyhue = function(a, b) {
-	var hsyA = Minerva.convert.rgb2hsy(a);
-	var hsyB = Minerva.convert.rgb2hsy(b);
-	return Minerva.convert.hsy2rgb({
-		h:hsyB.h,
-		s:hsyA.s,
-		y:hsyA.y
-	});
+Minerva.mixmodes.hwbhue = function(a, b) {
+	var hwbA = Minerva.convert.rgb2hwb([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hwbB = Minerva.convert.rgb2hwb([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hwb2rgb([hwbB[0], hwbA[1], hwbA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.hsysaturation = function(a, b) {
-	var hsyA = Minerva.convert.rgb2hsy(a);
-	var hsyB = Minerva.convert.rgb2hsy(b);
-	return Minerva.convert.hsy2rgb({
-		h:hsyA.h,
-		s:hsyB.s,
-		y:hsyA.y
-	});
+Minerva.mixmodes.hwbwhiteness = function(a, b) {
+	var hwbA = Minerva.convert.rgb2hwb([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hwbB = Minerva.convert.rgb2hwb([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hwb2rgb([hwbA[0], hwbB[1], hwbA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.hsyluminance = function(a, b) {
-	var hsyA = Minerva.convert.rgb2hsy(a);
-	var hsyB = Minerva.convert.rgb2hsy(b);
-	return Minerva.convert.hsy2rgb({
-		h:hsyA.h,
-		s:hsyA.s,
-		y:hsyB.y
-	});
+Minerva.mixmodes.hwbblackness = function(a, b) {
+	var hwbA = Minerva.convert.rgb2hwb([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hwbB = Minerva.convert.rgb2hwb([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hwb2rgb([hwbA[0], hwbA[1], hwbB[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.labluminance = function(a, b) {
-	var labA = Minerva.convert.rgb2lab(a);
-	var labB = Minerva.convert.rgb2lab(b);
-	return Minerva.convert.lab2rgb({
-		l:labB.l,
-		a:labA.a,
-		b:labA.b,
-	});
+Minerva.mixmodes.lablightness = function(a, b) {
+	var labA = Minerva.convert.rgb2lab([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var labB = Minerva.convert.rgb2lab([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.lab2rgb([labB[0], labA[1], labA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
 Minerva.mixmodes.labalpha = function(a, b) {
-	var labA = Minerva.convert.rgb2lab(a);
-	var labB = Minerva.convert.rgb2lab(b);
-	return Minerva.convert.lab2rgb({
-		l:labA.l,
-		a:labB.a,
-		b:labA.b,
-	});
+	var labA = Minerva.convert.rgb2lab([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var labB = Minerva.convert.rgb2lab([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.lab2rgb([labA[0], labB[1], labA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
-
 
 Minerva.mixmodes.labbeta = function(a, b) {
-	var labA = Minerva.convert.rgb2lab(a);
-	var labB = Minerva.convert.rgb2lab(b);
-	return Minerva.convert.lab2rgb({
-		l:labA.l,
-		a:labA.a,
-		b:labB.b,
-	});
+	var labA = Minerva.convert.rgb2lab([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var labB = Minerva.convert.rgb2lab([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.lab2rgb([labA[0], labA[1], labB[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
-Minerva.mixmodes.lchluminance = function(a, b) {
-	var lchA = Minerva.convert.rgb2lch(a);
-	var lchB = Minerva.convert.rgb2lch(b);
-	return Minerva.convert.lch2rgb({
-		l:lchB.l,
-		c:lchA.c,
-		h:lchA.h,
-	});
+Minerva.mixmodes.lchlightness = function(a, b) {
+	var lchA = Minerva.convert.rgb2lch([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var lchB = Minerva.convert.rgb2lch([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.lch2rgb([lchB[0], lchA[1], lchA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
 Minerva.mixmodes.lchchroma = function(a, b) {
-	var lchA = Minerva.convert.rgb2lch(a);
-	var lchB = Minerva.convert.rgb2lch(b);
-	return Minerva.convert.lch2rgb({
-		l:lchA.l,
-		c:lchB.c,
-		h:lchA.h,
-	});
+	var lchA = Minerva.convert.rgb2lch([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var lchB = Minerva.convert.rgb2lch([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.lch2rgb([lchA[0], lchB[1], lchA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
 
 Minerva.mixmodes.lchhue = function(a, b) {
-	var lchA = Minerva.convert.rgb2lch(a);
-	var lchB = Minerva.convert.rgb2lch(b);
-	return Minerva.convert.lch2rgb({
-		l:lchA.l,
-		c:lchA.c,
-		h:lchB.h,
-	});
+	var lchA = Minerva.convert.rgb2lch([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var lchB = Minerva.convert.rgb2lch([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.lch2rgb([lchA[0], lchA[1], lchB[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
 };
+
+Minerva.mixmodes.hsyhue = function(a, b) {
+	var hsyA = Minerva.convert.rgb2hsy([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hsyB = Minerva.convert.rgb2hsy([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsy2rgb([hsyB[0], hsyA[1], hsyA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+Minerva.mixmodes.hsysaturation = function(a, b) {
+	var hsyA = Minerva.convert.rgb2hsy([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hsyB = Minerva.convert.rgb2hsy([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsy2rgb([hsyA[0], hsyB[1], hsyA[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+Minerva.mixmodes.hsyluma = function(a, b) {
+	var hsyA = Minerva.convert.rgb2hsy([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var hsyB = Minerva.convert.rgb2hsy([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.hsy2rgb([hsyA[0], hsyA[1], hsyB[2]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+Minerva.mixmodes.cmykcyan = function(a, b) {
+	var cmykA = Minerva.convert.rgb2cmyk([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var cmykB = Minerva.convert.rgb2cmyk([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.cmyk2rgb([cmykB[0], cmykA[1], cmykA[2], cmykA[3]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+Minerva.mixmodes.cmykmagenta = function(a, b) {
+	var cmykA = Minerva.convert.rgb2cmyk([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var cmykB = Minerva.convert.rgb2cmyk([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.cmyk2rgb([cmykA[0], cmykB[1], cmykA[2], cmykA[3]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+Minerva.mixmodes.cmykyellow = function(a, b) {
+	var cmykA = Minerva.convert.rgb2cmyk([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var cmykB = Minerva.convert.rgb2cmyk([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.cmyk2rgb([cmykA[0], cmykA[1], cmykB[2], cmykA[3]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+Minerva.mixmodes.cmykkey = function(a, b) {
+	var cmykA = Minerva.convert.rgb2cmyk([(a.r * 255) & 255, (a.g * 255) & 255, (a.b * 255) & 255]);
+	var cmykB = Minerva.convert.rgb2cmyk([(b.r * 255) & 255, (b.g * 255) & 255, (b.b * 255) & 255]);
+	var theOut = Minerva.convert.cmyk2rgb([cmykA[0], cmykA[1], cmykA[2], cmykB[3]]);
+	return {
+		r:theOut[0] / 255,
+		g:theOut[1] / 255,
+		b:theOut[2] / 255,
+	}
+};
+
+
+
+/* Lighter luma */
+/* Darker luma */
+/* Lighter color */
+/* Darker color */
+
+/* Aliases */
+
